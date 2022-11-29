206a6afa98ad7dad61ee06cfddaaa080d0b06dcf
danja <danja@SAMUEL-GAMBINO>
2022-11-29 13:46:26 +0300

    Добавьте файлы проекта.

---
+++ Demoex2/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Demoex2.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demoex2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FrmMain.Navigate(new PageMain());
+        }
+
+        private void PasswordUser_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (PasswordUser.Text == "0000")
+            {
+                userName.Text = "Менеджер";
+            }
+            else 
+            {
+                userName.Text = "Пользователь";
+            }
+        }
+    }
+}
---
+++ Demoex2/Pages/PageMain.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Demoex2.AppDataFile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Demoex2.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageMain.xaml
+    /// </summary>
+    public partial class PageMain : Page
+    {
+        public PageMain()
+        {
+            InitializeComponent();
+            var currentAgents = GromykoBD_2Entities.GetContext().Agent.ToList();
+            LV.ItemsSource = currentAgents;
+            
+
+            //var currentHistory = GromykoBD_2Entities.GetContext().History_Sales.ToList();
+            //LV.ItemsSource = currentHistory;
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += UpdateData;
+            timer.Start();
+        }
+        
+
+        private void UpdateData(object sender, object e)
+        {
+           var holdrex =  GromykoBD_2Entities.GetContext().Agent.ToList();
+           foreach (var abs in holdrex)
+            {
+                var Coldrex = abs.History_Sales1.Select(a => a.Count_Product);
+                abs.Count = (int)Coldrex.Sum();
+            }    
+        }
+    }
+}
